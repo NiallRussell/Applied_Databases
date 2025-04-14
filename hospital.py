c8c4003804bc6df6f8d158074b26e9ca8769591f
Niall.Russell <niall.russell@ul.ie>
2025-04-14 20:05:55 +0100

    hospital.py

---
+++ hospital.py
@@ -0,0 +1,46 @@
+import pymysql
+import pymysql.cursors
+
+
+
+def main():
+    conn = pymysql.connect(
+        host ="localhost",
+        user = "root",
+        password = "root",
+        database = "hospital",
+        cursorclass=pymysql.cursors.DictCursor)
+    ppsn = input(f'Enter New Patient Details\nPPSN:')
+    fname = input(f'Enter New Patient Details\nFirst Name:')
+    sname = input(f'Enter New Patient Details\nSurname:')
+    add = input(f'Enter New Patient Details\nAddress:')
+    docid = input(f'Enter New Patient Details\nDoctor ID:')
+
+    values = (ppsn, fname, sname, add, docid)
+
+    if not isinstance(docid, int):
+        end_statement =  "Invalid value entered for Doctor ID"
+    with conn:
+        cursor = conn.cursor()
+        query_ppsn = "SELECT 1 FROM patient_table WHERE ppsn = %s LIMIT 1"
+        query_docid = "SELECT 1 FROM patient_table WHERE docid = %s LIMIT 1"
+        query_insert = "INSERT INTO patient_table(ppsn, first_name, surname, address, doctorID) VALUES (%s, %s, %s, %s, %s)"
+
+        cursor.execute(query_ppsn, (ppsn,))
+        result_ppsn = cursor.fetchone()
+
+        cursor.execute(query_docid, (docid,))
+        result_docid = cursor.fetchone()
+
+        cursor.execute(query_insert, values)
+
+        if result_ppsn or result_docid:
+            end_statement2 = "Existing PPSN, or non-existant DoctorID entered"
+
+    final_statement = " ".join(part for part in [end_statement, end_statement2] if part)
+    print(final_statement)
+
+if __name__ == "__main__":
+    main()
+
+
