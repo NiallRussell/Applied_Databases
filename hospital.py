6342502257528d047d2cf9440d8685fd0f4315da
Niall.Russell <niall.russell@ul.ie>
2025-04-16 11:34:37 +0100

    edit hospital

--- hospital.py
+++ hospital.py
@@ -18,27 +18,46 @@ def main():
 
     values = (ppsn, fname, sname, add, docid)
 
-    if not isinstance(docid, int):
-        end_statement =  "Invalid value entered for Doctor ID"
-    with conn:
-        cursor = conn.cursor()
-        query_ppsn = "SELECT 1 FROM patient_table WHERE ppsn = %s LIMIT 1"
-        query_docid = "SELECT 1 FROM patient_table WHERE docid = %s LIMIT 1"
-        query_insert = "INSERT INTO patient_table(ppsn, first_name, surname, address, doctorID) VALUES (%s, %s, %s, %s, %s)"
+    #query_ppsn = "SELECT 1 FROM patient_table WHERE ppsn = %s LIMIT 1"
+    #query_docid = "SELECT 1 FROM patient_table WHERE doctorID = %s LIMIT 1"
+    query_insert = "INSERT INTO patient_table(ppsn, first_name, surname, address, doctorID) VALUES (%s, %s, %s, %s, %s)"
 
-        cursor.execute(query_ppsn, (ppsn,))
-        result_ppsn = cursor.fetchone()
+    try:
+       with conn:   
+            cursor = conn.cursor()
+            cursor.execute(query_insert, values)
+            print("Insert Successful")
 
-        cursor.execute(query_docid, (docid,))
-        result_docid = cursor.fetchone()
+    except pymysql.err.IntegrityError as e:
+            error_code = e.args[0]
+            error_msg = str(e)
 
-        cursor.execute(query_insert, values)
+            if (error_code == 1062 and "ppsn" in error_msg) or (error_code == 1452 and "doctorID" in error_msg):
+                 print("Existing PPSN, or non-existant DoctorID entered")
+            else:
+                 print("Integrity Error:", e)
 
-        if result_ppsn or result_docid:
-            end_statement2 = "Existing PPSN, or non-existant DoctorID entered"
+    except pymysql.DataError as e:
+            error_code = e.args[0]
+            print("Invalid value entered for Doctor ID")
+   # if not isinstance(docid, int):
+       # end_statement =  "Invalid value entered for Doctor ID"
 
-    final_statement = " ".join(part for part in [end_statement, end_statement2] if part)
-    print(final_statement)
+          
+
+           # cursor.execute(query_ppsn, (ppsn,))
+           # result_ppsn = cursor.fetchone()
+
+        #cursor.execute(query_docid, (docid,))
+      #  result_docid = cursor.fetchone()
+        
+      #  cursor.execute(query_insert, values)
+
+        #if result_ppsn or result_docid:
+           # end_statement2 = "Existing PPSN, or non-existant DoctorID entered"
+
+    #final_statement = " ".join(part for part in [end_statement, end_statement2] if part)
+    #print(final_statement)
 
 if __name__ == "__main__":
     main()
