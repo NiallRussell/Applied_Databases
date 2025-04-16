34cfe987490c4be437f05c4853ee93bd25599701
Niall.Russell <niall.russell@ul.ie>
2025-04-16 15:26:58 +0100

    cursor

--- hospital.py
+++ hospital.py
@@ -10,26 +10,33 @@ def main():
         password = "root",
         database = "hospital",
         cursorclass=pymysql.cursors.DictCursor)
-    ppsn = input(f'Enter New Patient Details\nPPSN:')
-    fname = input(f'Enter New Patient Details\nFirst Name:')
-    sname = input(f'Enter New Patient Details\nSurname:')
-    add = input(f'Enter New Patient Details\nAddress:')
-    docid = input(f'Enter New Patient Details\nDoctor ID:')
+    
+    choice = int(input(f'''Please select an option:
+                   Type 1 to input patient details
+                   Type 2 to search patient details by surname (partial entries allowed)
+                   '''))
 
-    values = (ppsn, fname, sname, add, docid)
+    if choice == 1:
+        ppsn = input(f'Enter New Patient Details\nPPSN:')
+        fname = input(f'Enter New Patient Details\nFirst Name:')
+        sname = input(f'Enter New Patient Details\nSurname:')
+        add = input(f'Enter New Patient Details\nAddress:')
+        docid = input(f'Enter New Patient Details\nDoctor ID:')
+
+        values = (ppsn, fname, sname, add, docid)
 
     #query_ppsn = "SELECT 1 FROM patient_table WHERE ppsn = %s LIMIT 1"
     #query_docid = "SELECT 1 FROM patient_table WHERE doctorID = %s LIMIT 1"
-    query_insert = "INSERT INTO patient_table(ppsn, first_name, surname, address, doctorID) VALUES (%s, %s, %s, %s, %s)"
+        query_insert = "INSERT INTO patient_table(ppsn, first_name, surname, address, doctorID) VALUES (%s, %s, %s, %s, %s)"
 
-    try:
-       with conn:   
-            cursor = conn.cursor()
-            cursor.execute(query_insert, values)
-            conn.commit()
-            print("Insert Successful")
+        try:
+            with conn:   
+                cursor = conn.cursor()
+                cursor.execute(query_insert, values)
+                conn.commit()
+                print("Insert Successful")
 
-    except pymysql.err.IntegrityError as e:
+        except pymysql.err.IntegrityError as e:
             error_code = e.args[0]
             error_msg = str(e)
 
@@ -38,9 +45,28 @@ def main():
             else:
                  print("Integrity Error:", e)
 
-    except pymysql.DataError as e:
-            error_code = e.args[0]
-            print("Invalid value entered for Doctor ID")
+        except pymysql.DataError as e:
+                error_code = e.args[0]
+                print("Invalid value entered for Doctor ID")
+    
+    elif choice == 2:
+        pname = input("Enter patient surname- input partial surname where full surname unknown:")
+        query_search = "SELECT * from patient_table WHERE surname LIKE %s"
+        try:
+            with conn:
+                with conn.cursor() as cursor:
+                    cursor.execute(query_search, (f"%{pname}%"))
+                    results = cursor.fetchall()
+
+                    for row in results:
+                        print(row)
+
+        except pymysql.MySQLError as e:
+            print("Database error:", e)
+        except Exception as e:
+            print("Unexpected error:", e)
+
+
    # if not isinstance(docid, int):
        # end_statement =  "Invalid value entered for Doctor ID"
 
